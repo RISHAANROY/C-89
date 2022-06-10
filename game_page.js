41f2694fdab760053961c9ffb710149d6c10e94a
RISHAANROY <96765661+RISHAANROY@users.noreply.github.com>
2022-06-10 20:32:40 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,26 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+
+player1_score = 0;
+player2_score = 0;
+
+
+document.getElementById("player1_name").innerHTML = player1_name +" : ";
+document.getElementById("player2_name").innerHTML = player2_name +" : ";
+
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player1_score ;
+
+
+document.getElementById("player_Question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_Answer").innerHTML = "Answer Turn - " + player2_name;
+
+
+function send() 
+{
+ 
+ get_word = document.getElementById("word").value;
+ word = get_word.toLowerCase();
+ console.log    
